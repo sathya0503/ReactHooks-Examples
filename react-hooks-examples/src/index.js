2e9ecd4ec52eacca7da33ee50ff1f68bde68a589
sathya0503 <133220637+sathya0503@users.noreply.github.com>
2024-08-06 06:47:26 +0530

    commit boostart and fontawesome changes
    To Install md bootstrap -
    npm i mdbootstrap
    To Install fontawesome -
    npm i @fortawesome/fontawesome-free
    
    Add following in index.css file-
    import '@fortawesome/fontawesome-free';
    import 'mdbootstrap/css/mdb.css';
    import 'mdbootstrap/css/bootstrap.css';
    
    Add following links in Index.html
    <link rel="fontawesome-sheet" href="https://use.fontawesome.com/releases/v6.5.2/css/all.css" />
        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />

--- react-hooks-examples/src/index.js
+++ react-hooks-examples/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free';
+import 'mdbootstrap/css/mdb.css';
+import 'mdbootstrap/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
